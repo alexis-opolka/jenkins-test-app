f04653c08518b0d9f22c04ef5141a22596284297
alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
2025-02-03 20:37:36 +0100

    Fixed sh incorrect syntax
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>

--- sample-app.sh
+++ sample-app.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+tempdir=".jenkins_sample"
 
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
+cleanTemp() {
+    if [ -d $tempdir ];  then
+        ### We're cleaning the temporary directory
+        rm -fr $tempdir
+    fi
+}
 
-cat << EOF > /tempdir/Dockerfile
+cleanTemp
+
+if [ ! $1 ]; then
+    echo "I need at least the name of the container!";
+    exit 1
+fi;
+
+mkdir $tempdir
+mkdir $tempdir/templates
+mkdir $tempdir/static
+
+cp sample_app.py $tempdir/.
+cp -r templates/* $tempdir/templates/.
+cp -r static/* $tempdir/static/.
+
+cat << EOF > $tempdir/Dockerfile
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
@@ -18,7 +34,15 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 EOF
 
-cd tempdir
+oldDir=$(pwd)
+
+cd $tempdir
+
 docker build -t jenkins-sample:latest .
 docker run -t -d -p 5050:5050 --name $1 jenkins-sample:latest
-docker ps -a 
+docker ps -a
+
+cd $oldDir
+
+
+cleanTemp
